01790e67dff7732c36f685803c0573478c742b1b
7-- <regop412@gmail.com>
2016-11-01 17:05:39 -0400

    test project up. Complete data.associates migration.

--- Workforce.Data.Associates2/Workforce.Data.Associates2.Soap/AssociateService.svc.cs
+++ Workforce.Data.Associates2/Workforce.Data.Associates2.Soap/AssociateService.svc.cs
@@ -14,7 +14,7 @@ namespace Workforce.Data.Associates2.Soap
 {
   // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
   // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-  public class Service1 : IAssociateService
+  public class AssociateService : IAssociateService
   {
     private readonly AccessHelper ac = new AccessHelper();
     private readonly EfMapper emap = new EfMapper();
