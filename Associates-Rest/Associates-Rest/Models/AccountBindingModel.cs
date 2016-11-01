ee854e77de53528468117493c7c5df982e7e9706
wlopez <wlopez.career@gmail.com>
2016-11-01 14:28:00 -0400

    Migrated more files from the old repo

---
+++ Associates-Rest/Associates-Rest/Models/AccountBindingModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Associates.Rest.Models
+{
+  /// <summary>
+  /// The model that we will use
+  /// to create a user. 
+  /// </summary>
+  public class CreateUserBindingModel
+  {
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Username")]
+    public string Username { get; set; }
+
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Display(Name = "Role Name")]
+    public string RoleName { get; set; }
+
+    [Required]
+    [Display(Name = "Batch")]
+    public int Batch { get; set; }
+
+    [Required]
+    [Display(Name = "Gender")]
+    public string Gender { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+  }
+
+  /// <summary>
+  /// The model that we will be using
+  /// to change a user's password
+  /// </summary>
+  public class ChangePasswordBindingModel
+  {
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Current Password")]
+    public string OldPassword { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage="The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    [Display(Name = "New Password")]
+    public string NewPassword { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    [Compare("NewPassword", ErrorMessage = "Your passwords do not match. Please check to make sure that they match.")]
+    public string ConfirmPassword { get; set; }
+  }
+}
