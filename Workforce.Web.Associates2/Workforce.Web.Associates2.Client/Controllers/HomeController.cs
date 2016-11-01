17804c85c21d14eb704d7ccb01d43240c0c7150e
Sam Choo <samchoo@outlook.com>
2016-11-01 14:08:55 -0400

    Add HomeController.cs and Shared/_Layout.cshtml blank files.

---
+++ Workforce.Web.Associates2/Workforce.Web.Associates2.Client/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Workforce.Web.Associates2.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
