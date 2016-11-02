481f1877b0b73426e808b0950cab86dff437e508
brandon.parnoff <brandon.parnoff@outlook.com>
2016-11-02 13:54:42 -0400

    Added Domain Models

---
+++ Associates-Rest/Associates-Domain/DomainModels/GenderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Felice.Domain.DomainModels
+{
+   public class GenderDto
+   {
+      public int GenderID { get; set; }
+      [Required, RegularExpression("/^[A-Za-Z]+$/")]
+      public string Name { get; set; }
+   }
+}
