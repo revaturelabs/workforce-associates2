918e25a9ad75d9a4ccac1ca90f8844b1b01a6f03
wlopez <wlopez.career@gmail.com>
2016-11-04 11:41:34 -0400

    Service References for soap services are up and running. Logic API should be up after this. Now working on security side some more

---
+++ Workforce.Logic.Associates2/Workforce.Logic.Associates2.Domain/CoreValidator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Associates2.Domain
+{
+   public class CoreValidator
+   {
+      public bool ValidateRequiredString(string newString, int maxLength)
+      {
+         if (String.IsNullOrWhiteSpace(newString) || newString.Length > maxLength)
+         {
+            return false;
+         }
+         else
+         {
+            return true;
+         }
+      }
+
+      public bool ValidateStandardString(string newString)
+      {
+         if (newString == String.Empty)
+         {
+            return false;
+         }
+         else
+         {
+            return true;
+         }
+      }
+
+      public bool ValidateInt(int newInt)
+      {
+         if (newInt < 0)
+         {
+            return false;
+         }
+
+         return true;
+      }
+
+      public bool ValidateNullableKey(Nullable<int> nullableInt)
+      {
+         if (nullableInt == null || nullableInt >= 0)
+         {
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+      }
+
+      public bool ValidateBool(bool? newBool)
+      {
+         if (newBool != true && newBool != false && newBool != null)
+         {
+            return false;
+         }
+         else
+         {
+            return true;
+         }
+      }
+
+      public bool ValidateDate(DateTime? newDate)
+      {
+         DateTime test;
+
+         if (newDate == null)
+         {
+            return true;
+         }
+         else if (DateTime.TryParse(newDate.ToString(), out test))
+         {
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+      }
+   }
+}
---
+++ Workforce.Logic.Associates2/Workforce.Logic.Associates2/Controllers/InstructorController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Workforce.Logic.Associates2.Domain;
+using Workforce.Logic.Associates2.Domain.DomainModels;
+
+namespace Workforce.Logic.Associates2.Rest.Controllers
+{
+    public class InstructorController : ApiController
+    {
+      private readonly LogicHelper logic = new LogicHelper();
+
+      /// <summary>
+      /// This 'Get' method returns all Instructors regardless of status
+      /// </summary>
+      [HttpGet]
+      public async Task<HttpResponseMessage> FindAll()
+      {
+         return Request.CreateResponse(HttpStatusCode.OK, await logic.GetAllInstructors());
+      }
+
+      /// <summary>
+      /// This is the base 'Get' method for Instructors that retrieves based on status
+      /// </summary>
+      [HttpGet]
+      public async Task<HttpResponseMessage> FindByStatus(string status)
+      {
+         return Request.CreateResponse(HttpStatusCode.OK, await logic.GetInstructorsByStatus(status));
+      }
+
+      /// <summary>
+      /// Adds a new Instructor to the database
+      /// </summary>
+      [HttpPost]
+      public async Task<HttpResponseMessage> Post([FromBody]InstructorDto instructor)
+      {
+         return Request.CreateResponse(HttpStatusCode.OK, await logic.AddNewInstructor(instructor));
+      }
+
+      /// <summary>
+      /// Deactivates (Deletes) an active Instructor
+      /// Must confirm if this will reactivate it too
+      /// </summary>
+      [HttpDelete]
+      public async Task<HttpResponseMessage> Delete([FromBody]InstructorDto instructor)
+      {
+         return Request.CreateResponse(HttpStatusCode.OK, await logic.DeactivateInstructor(instructor));
+      }
+
+      /// <summary>
+      /// takes in key information to update a specific Instructor
+      /// </summary>
+      [HttpPut]
+      public async Task<HttpResponseMessage> Put([FromBody]InstructorDto instructor)
+      {
+         return Request.CreateResponse(HttpStatusCode.OK, await logic.UpdateInstructor(instructor));
+      }
+   }
+}
