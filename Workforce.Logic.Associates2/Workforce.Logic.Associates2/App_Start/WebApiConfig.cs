2128eae29394c28de160eca492c27b4d4308a27e
brandon.parnoff <brandon.parnoff@outlook.com>
2016-11-16 11:43:55 -0500

    restored routing

--- Workforce.Logic.Associates2/Workforce.Logic.Associates2/App_Start/WebApiConfig.cs
+++ Workforce.Logic.Associates2/Workforce.Logic.Associates2/App_Start/WebApiConfig.cs
@@ -9,16 +9,16 @@ namespace Workforce.Logic.Associates2
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+         // Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+         // Web API routes
+         config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
+         config.Routes.MapHttpRoute(
+               name: "DefaultApi",
+               routeTemplate: "api/{controller}/{action}/{status}",
+               defaults: new { controller = "index", action = RouteParameter.Optional, status = RouteParameter.Optional }
+         );
+      }
     }
 }
