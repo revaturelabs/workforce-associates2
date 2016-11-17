ce700d80b6f8626a4861fd5306e9ff4b7cc9bf94
wlopez <wlopez.career@gmail.com>
2016-11-17 11:22:59 -0500

    created the logger class and made some configuration in the config file

---
+++ Workforce.Logic.Associates2/Workforce.Logic.Associates2/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: log4net.Config.XmlConfigurator(Watch=true)]
+
+namespace Workforce.Logic.Associates2
+{
+  public class Logger
+  {
+    //reflection can be slow
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+  }
+}
